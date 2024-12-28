bce89f9fc1cc4942c0c321314ad9f0eb07aff8e0
Gaurav Prakash <prakashgaurav189@gmail.com>
2024-12-28 16:38:20 +0530

    Learning Node

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # BACKEND-LEARNING
+Command for to push things 
+1. git branch -M main
+2. git push -u origin main
+
+
+# Package 
+A reusable code that other developer codes it , then publish it on the website(npm) and we use it using it.
+
+# Node 
+A runtime enviroment where JS codes runs. It is chrome V8 engine which is pulled off the chrome and add some extra functionality in it.
+
+# Express
---
+++ node/app.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const server = http.createServer((req,res)=>{
+    // console.log(req.url);
+    // res.end("Hello World");
+    if(req.url === "/about"){
+        res.end("The about page");
+    }
+    if(req.url === "/profile"){
+        res.end("The profile page");
+    }
+    if(req.url === "/"){
+        res.end("The home page");
+    }
+})
+
+server.listen(3000,()=>{
+    console.log("Server is listening on the port 3000");
+})
